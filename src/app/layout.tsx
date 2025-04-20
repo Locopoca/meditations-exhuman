828f51a2fbb0fb13ae0b9b46ec40a4072b145dcd
Ozzy <oskar.langee@gmail.com>
2025-04-20 11:31:18 +0200

    ten

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { headers } from 'next/headers'
 import { type ReactNode } from 'react'
 import { cookieToInitialState } from 'wagmi'
 
-import { getConfig } from '../wagmi'
+// import { getConfig } from '../wagmi'
 import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,14 +19,14 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout(props: { children: ReactNode }) {
-  const initialState = cookieToInitialState(
-    getConfig(),
-    headers().get('cookie'),
-  )
+  // const initialState = cookieToInitialState(
+  //   // getConfig(),
+  //   // headers().get('cookie'),
+  // )
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers initialState={initialState}>{props.children}</Providers>
+        <Providers>{props.children}</Providers>
       </body>
     </html>
   )
