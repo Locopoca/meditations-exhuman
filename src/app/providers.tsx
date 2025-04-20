1cde01d456492b7c60b09625041094771878467d
Ozzy <oskar.langee@gmail.com>
2025-04-20 11:27:49 +0200

    nine

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,22 +2,22 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { type ReactNode, useState } from 'react'
-import { type State, WagmiProvider } from 'wagmi'
+// import { type State, WagmiProvider } from 'wagmi'
 
-import { getConfig } from '@/wagmi'
+// import { getConfig } from '@/wagmi'
 
 export function Providers(props: {
   children: ReactNode
-  initialState?: State
+  // initialState?: State
 }) {
-  const [config] = useState(() => getConfig())
+  // const [config] = useState(() => getConfig())
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <WagmiProvider config={config} initialState={props.initialState}>
+    // <WagmiProvider config={config} initialState={props.initialState}>
       <QueryClientProvider client={queryClient}>
         {props.children}
       </QueryClientProvider>
-    </WagmiProvider>
+    // </WagmiProvider>
   )
 }
