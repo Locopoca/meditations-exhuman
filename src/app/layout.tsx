c10a5fbf13868436b3aef89014780027097ac0d1
Ozzy <oskar.langee@gmail.com>
2025-04-20 11:32:41 +0200

    ten

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { headers } from 'next/headers'
 import { type ReactNode } from 'react'
-import { cookieToInitialState } from 'wagmi'
+// import { cookieToInitialState } from 'wagmi'
 
 // import { getConfig } from '../wagmi'
 import { Providers } from './providers'
